24a6b7923ed609723dbab9b5f9aff7671539259b
Jacob Leon Lyerla <umasour@yahoo.com>
2020-10-20 21:13:36 -0700

    more sample code to test react-calendar

--- src/components/calender.js
+++ src/components/calender.js
@@ -12,12 +12,21 @@ class MyCalender extends Component {
     
 }
   render() {
+    const { value } = this.state;
     return (
       <div>
-        <Calendar
-          onChange={this.onChange}
-          value={this.state.date}
-        />
+             <header>
+          <h1>react-calendar sample page</h1>
+        </header>
+        <div className="Sample__container">
+          <main className="Sample__container__content">
+            <Calendar
+              onChange={this.onChange}
+              showWeekNumbers
+              value={value}
+            />
+          </main>
+        </div>
       </div>
     );
   }
