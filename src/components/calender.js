f533bb5878832589a296043571e9617aca8bc3b6
Jacob Leon Lyerla <umasour@yahoo.com>
2020-10-20 20:48:14 -0700

    created new calendar component

---
+++ src/components/calender.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Calendar from 'react-calendar';
+ 
+class MyApp extends Component {
+  state = {
+    date: new Date(),
+  }
+ 
+  onChange = date => this.setState({ date })
+ 
+  render() {
+      console.log(this.state.date)
+    return (
+      <div>
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+        />
+      </div>
+    );
+  }
+}
+export default Calendar;
