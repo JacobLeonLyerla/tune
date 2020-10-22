d6c2b5ae48cda4e01f7e41cc1c2bf14d846cce95
Jacob Leon Lyerla <umasour@yahoo.com>
2020-10-21 17:49:10 -0700

    tinkering with props for calendar

--- src/components/calender.js
+++ src/components/calender.js
@@ -1,29 +1,24 @@
-import React, { Component } from 'react';
-import Calendar from 'react-calendar';
-import '../css/calendar.css'
+import React, { Component } from "react";
+import Calendar from "react-calendar";
+import "../css/calendar.css";
 class MyCalender extends Component {
   state = {
     date: new Date(),
-  }
- 
-  onChange = date => {
-    console.log(date)
-    this.setState({ date })
-    
-}
+  };
+
+  onChange = (date) => {
+    this.setState({ date });
+  };
   render() {
     const { value } = this.state;
     return (
-      <div  className="Sample" >
-             <header>
+      <div className="Sample">
+        <header>
           <h1>react-calendar sample page</h1>
         </header>
         <div className="Sample__container">
           <main className="Sample__container__content">
-            <Calendar
-              onChange={this.onChange}
-              value={value}
-            />
+            <Calendar onChange={this.onChange} value={value} activeStartDate={new Date(207, 0, 1)} />
           </main>
         </div>
       </div>
