2e0d69cf123a36e5d6da65c7cd1e8e56288d7222
Jacob Leon Lyerla <umasour@yahoo.com>
2020-10-22 21:43:27 -0700

    Not sure how to render this I will figure it out tomorrow

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Calender from "./components/calender";
 import {routes} from "./helpers/appRouters"
+import {Link} from "react-router-dom"
 import "./App.css";
 function App() {
+  console.log(routes)
   return (
     <div className="App">
-      {routes}
+      {routes()}
       <Calender />
-      
+      <Link style={{ textDecoration: "none", color: "black" }} to="/test">
+
+            <button> Test </button>
+        </Link>
     </div>
   );
 }
