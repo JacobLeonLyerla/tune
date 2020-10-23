d7f113f337b65b691f4d185e5bc9a214df71644b
Jacob Leon Lyerla <umasour@yahoo.com>
2020-10-22 21:38:49 -0700

    trying to render routes without binding it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Calender from "./components/calender";
+import {routes} from "./helpers/appRouters"
 import "./App.css";
 function App() {
   return (
     <div className="App">
+      {routes}
       <Calender />
+      
     </div>
   );
 }
