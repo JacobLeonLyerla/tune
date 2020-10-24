b36c608ae48659d6139c07bd1bc436a4ccafc658
Jacob Leon Lyerla <umasour@yahoo.com>
2020-10-23 21:52:19 -0700

    removed header

--- src/components/calender.js
+++ src/components/calender.js
@@ -13,9 +13,7 @@ class MyCalender extends Component {
     const { value } = this.state;
     return (
       <div className="Sample">
-        <header>
-          <h1>react-calendar sample page</h1>
-        </header>
+
         <div className="Sample__container">
           <main className="Sample__container__content">
             <Calendar onChange={this.onChange} value={value}/>
