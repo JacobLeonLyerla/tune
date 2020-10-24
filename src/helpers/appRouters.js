429512ca2abbe8df5677b561072271daf553b458
Jacob Leon Lyerla <umasour@yahoo.com>
2020-10-23 21:52:00 -0700

    router fixed and workin

--- src/helpers/appRouters.js
+++ src/helpers/appRouters.js
@@ -2,5 +2,5 @@ import React, {Fragment} from "react";
 import {Route} from "react-router-dom"
 
 export  function routes(){
-    return<Route exact path="/Test"render={<div>Hello Wold</div>}/>
+    return<Route exact path="/Test" render={()=><div>Hello Wold</div>}/>
 }
