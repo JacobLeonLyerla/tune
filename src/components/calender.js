818f6ccd9bbffd726c64ec5b5c80c9f237548f14
Jacob Leon Lyerla <umasour@yahoo.com>
2020-10-27 23:40:47 -0700

    working on converting this code to hooks, Still learning hooks better

--- src/components/calender.js
+++ src/components/calender.js
@@ -1,25 +1,26 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "../css/calendar.css";
-class MyCalender extends Component {
-  state = {
-    date: new Date(),
-  };
 
-  onChange = (date) => {
+ const onChange = (date) => {
     this.setState({ date });
   };
-  render() {
+  
+ function MyCalender() {
+ const [date] = useState();
+
+ 
+
     const { value } = this.state;
     return (
       <div className="Sample">
         <div className="Sample__container">
           <main className="Sample__container__content">
-            <Calendar onChange={this.onChange} value={value} />
+            <Calendar onChange={onChange} value={value} />
           </main>
         </div>
       </div>
     );
   }
-}
+
 export default MyCalender;
