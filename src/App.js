f87ea87fde4de1d6142bf16cdb183139f772698d
Jacob Leon Lyerla <umasour@yahoo.com>
2020-10-28 22:37:36 -0700

    changed the bottons

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import { routes } from "./helpers/appRouters";
 import { Link } from "react-router-dom";
 import "./App.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import TuneProvider, {TuneContext} from "./contexts/tuneProvider"
+import { faToggleOn,faToggleOff } from '@fortawesome/free-solid-svg-icons'
+
 function App() {
   console.log(routes);
   return (
     // I want to figure out how to make the button toggle instead of needing two buttons
     <div className="App">
-       <FontAwesomeIcon icon={faCoffee} />
-      <Link style={{ textDecoration: "none", color: "black" }} to="/test">
-        <button> ON </button>
-      </Link>
+     
       <Link style={{ textDecoration: "none", color: "black" }} to="/">
-        <button> OFF </button>
-      </Link>{" "}
+      <FontAwesomeIcon icon={faToggleOff} />
+
+      </Link>{" "} <Link style={{ textDecoration: "none", color: "black" }} to="/calendar">
+      <FontAwesomeIcon icon={faToggleOn} />
+      </Link>
       {routes()}
     </div>
   );
