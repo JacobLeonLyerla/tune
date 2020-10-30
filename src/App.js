3e1c1aadf0cfdec79bddd4a3890fa9bbb4ab7c17
Jacob Leon Lyerla <umasour@yahoo.com>
2020-10-29 23:47:15 -0700

    setting up consitional rnedering

--- src/App.js
+++ src/App.js
@@ -6,14 +6,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faToggleOn, faToggleOff } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-  const [address, setAddress] = useState("/");
+  const [address, setAddress] = useState("/calendar");
   const [icon, setIcon] = useState(faToggleOff);
+
+  const changeIcon=()=>{
+  if(address === "/calendar"){
+    setAddress("/")
+    setIcon(faToggleOn)
+  }else{
+    setAddress("/calendar")
+    setIcon(faToggleOff)
+  }
+
+  }
 
   return (
     // I want to figure out how to make the button toggle instead of needing two buttons
     <div className="App">
       
-      <Link style={{ textDecoration: "none", color: "black" }} to={address}>
+      <Link onClick={changeIcon()} style={{ textDecoration: "none", color: "black" }} to={address}>
         <FontAwesomeIcon icon={icon} />
       </Link>{" "}
     
