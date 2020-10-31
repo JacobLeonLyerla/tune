a0d3bd982907ed9cc06bcf24c425761c3acb382e
Jacob Leon Lyerla <umasour@yahoo.com>
2020-10-30 21:50:46 -0700

    rendered the message I wanted component working

--- src/components/reactIntl.js
+++ src/components/reactIntl.js
@@ -1,38 +1,53 @@
-import {createIntl, createIntlCache} from 'react-intl'
+import React from "react";
+import { createIntl, createIntlCache } from "react-intl";
+function ReactIntl() {
+  // Translated messages in French with matching IDs to what you declared
+  const messagesInFrench = {
+    myMessage: "Aujourd'hui, c'est le {ts, date, ::yyyyMMdd}",
+  };
 
-// Translated messages in French with matching IDs to what you declared
-const messagesInFrench = {
-  myMessage: "Aujourd'hui, c'est le {ts, date, ::yyyyMMdd}",
-}
+  // This is optional but highly recommended
+  // since it prevents memory leak
+  const cache = createIntlCache();
 
-// This is optional but highly recommended
-// since it prevents memory leak
-const cache = createIntlCache()
+  // Create the `intl` object
+  const intl = createIntl(
+    {
+      // Locale of the application
+      locale: "fr",
+      // Locale of the fallback defaultMessage
+      defaultLocale: "en",
+      messages: messagesInFrench,
+    },
+    cache
+  );
 
-// Create the `intl` object
-const intl = createIntl(
-  {
-    // Locale of the application
-    locale: 'fr',
-    // Locale of the fallback defaultMessage
-    defaultLocale: 'en',
-    messages: messagesInFrench,
-  },
-  cache
-)
+  // Aujourd'hui, c'est le 23/07/2020
+  console.log(
+    intl.formatMessage(
+      {
+        // Matching ID as above
+        id: "myMessage",
+        // Default Message in English
+        defaultMessage: "Today is {ts, date, ::yyyyMMdd}",
+      },
+      { ts: Date.now() }
+    )
+  );
 
-// Aujourd'hui, c'est le 23/07/2020
-console.log(
-  intl.formatMessage(
+  // 19,00 €
+  console.log(intl.formatNumber(19, { style: "currency", currency: "EUR" }));
+
+  return(
+  <div>{ intl.formatMessage(
     {
       // Matching ID as above
-      id: 'myMessage',
+      id: "myMessage",
       // Default Message in English
-      defaultMessage: 'Today is {ts, date, ::yyyyMMdd}',
+      defaultMessage: "Today is {ts, date, ::yyyyMMdd}",
     },
-    {ts: Date.now()}
+    { ts: Date.now() }
+  )}</div>
   )
-)
-
-// 19,00 €
-console.log(intl.formatNumber(19, {style: 'currency', currency: 'EUR'}))
+}
+export default ReactIntl;
