bcec0f3645d252083dd0393dd8ccef525d1513f4
Jacob Leon Lyerla <umasour@yahoo.com>
2020-10-30 21:51:06 -0700

    added route for react intl

--- src/helpers/appRouters.js
+++ src/helpers/appRouters.js
@@ -1,7 +1,13 @@
 import React, { Fragment } from "react";
 import Calendar from "../components/calender";
+import ReactIntl from "../components/reactIntl";
 import { Route } from "react-router-dom";
 
 export function routes() {
-  return <Route exact path="/calendar" render={() => <Calendar />} />;
+  return (
+    <Fragment>
+      <Route exact path="/calendar" render={() => <Calendar />} />
+      <Route exact path="/" render={() => <ReactIntl />} />
+    </Fragment>
+  );
 }
