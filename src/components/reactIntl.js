fcc05d1d48607c30726e19f5f8e1b6785917f015
Jacob Leon Lyerla <umasour@yahoo.com>
2020-11-02 22:51:10 -0700

    formatting

--- src/components/reactIntl.js
+++ src/components/reactIntl.js
@@ -1,26 +1,22 @@
 import React from "react";
-import {IntlProvider, FormattedMessage, FormattedNumber} from 'react-intl'
+import { IntlProvider, FormattedMessage, FormattedNumber } from "react-intl";
 function ReactIntl() {
   // Translated messages in French with matching IDs to what you declared
   const messagesInEnglish = {
     myMessage: "Current Date: {ts, date, ::yyyyMMdd}",
   };
 
-
-
-
-  return(
+  return (
     <IntlProvider messages={messagesInEnglish} locale="en" defaultLocale="en">
-    <p>
-      <FormattedMessage
-        id="myMessage"
-        defaultMessage="Today is {ts, date, ::yyyyMMdd}"
-        values={{ts: Date.now()}}
-      />
-      <br />
-      <FormattedNumber value={19} style="currency" currency="USD" />
-    </p>
-  </IntlProvider>
-  )
+      <p>
+        <FormattedMessage
+          id="myMessage"
+          defaultMessage="Today is {ts, date, ::yyyyMMdd}"
+          values={{ ts: Date.now() }}
+        />
+        <FormattedNumber value={19} style="currency" currency="USD" />
+      </p>
+    </IntlProvider>
+  );
 }
 export default ReactIntl;
