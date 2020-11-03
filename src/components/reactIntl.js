c5c3dc10a615c26a40619783f885bdafd538e806
Jacob Leon Lyerla <umasour@yahoo.com>
2020-11-02 22:47:05 -0700

    testing our react components for intl

--- src/components/reactIntl.js
+++ src/components/reactIntl.js
@@ -1,39 +1,26 @@
 import React from "react";
-import { createIntl, createIntlCache } from "react-intl";
+import {IntlProvider, FormattedMessage, FormattedNumber} from 'react-intl'
 function ReactIntl() {
   // Translated messages in French with matching IDs to what you declared
   const messagesInEnglish = {
     myMessage: "Current Date: {ts, date, ::yyyyMMdd}",
   };
 
-  // This is optional but highly recommended
-  // since it prevents memory leak
-  const cache = createIntlCache();
 
-  // Create the `intl` object
-  const intl = createIntl(
-    {
-      // Locale of the application
-      locale: "en",
-      // Locale of the fallback defaultMessage
-      defaultLocale: "en",
-      messages: messagesInEnglish,
-    },
-    cache
-  );
 
-  console.log(intl.formatNumber(19, { style: "currency", currency: "USD" }));
 
   return(
-  <div>{ intl.formatMessage(
-    {
-      // Matching ID as above
-      id: "myMessage",
-      // Default Message in English
-      defaultMessage: "Today is {ts, date, ::yyyyMMdd}",
-    },
-    { ts: Date.now() }
-  )}</div>
+    <IntlProvider messages={messagesInEnglish} locale="en" defaultLocale="en">
+    <p>
+      <FormattedMessage
+        id="myMessage"
+        defaultMessage="Today is {ts, date, ::yyyyMMdd}"
+        values={{ts: Date.now()}}
+      />
+      <br />
+      <FormattedNumber value={19} style="currency" currency="USD" />
+    </p>
+  </IntlProvider>
   )
 }
 export default ReactIntl;
