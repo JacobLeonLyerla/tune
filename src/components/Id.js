1e22a1ddd74f90950f47f4d0eeca196a2d86a472
Jacob Leon Lyerla <umasour@yahoo.com>
2020-11-04 23:40:32 -0700

    test Id rendering

--- src/components/Id.js
+++ src/components/Id.js
@@ -3,12 +3,14 @@ import nextId, { useId } from "react-id-generator";
  
 const ID = ({ children, ...rest }) => {
   const [htmlId] = useId();
+  
+const id1 = nextId()
  
   return (
     <div>
       <label htmlFor={htmlId}>{children}</label>
       <input id={htmlId} type="radio" {...rest} />
-      {nextId}
+      {id1}
     </div>
   );
 };
