83a8756fedf3f29deb9c436befaa9a7a3de12b81
Jacob Leon Lyerla <umasour@yahoo.com>
2020-11-06 20:18:01 -0700

    effect is like component did mount but for hooks

--- src/components/calender.js
+++ src/components/calender.js
@@ -1,19 +1,26 @@
-import React, { useState,useContext } from "react";
+import React, { useState,useContext, useEffect } from "react";
 import Calendar from "react-calendar";
 import "../css/calendar.css";
 import TuneContext from "../contexts/tuneProvider"
 
 function MyCalender() {
   const [date, setDate] = useState(new Date());
+  const [text, setText] = useState("aa")
   const test = useContext(TuneContext)
+
   const onChange = (date) => {
  
     setDate(date);
   };
+  useEffect(() => {
+    console.log("effect ran")
+   
+     setText(test.testfunction("test completed"))
+  });
 
   return (
     <div className="Sample">
-      <div>{test.testfunction()}</div>
+      <div>{text}</div>
       <div className="Sample__container">
         <main className="Sample__container__content">
           <Calendar onChange={onChange} value={date} />
